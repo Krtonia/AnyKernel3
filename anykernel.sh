ae38108bfa0e423c5958ba5953e56b0e3591757a
Shikhar <krtonia@gmail.com>
2022-12-05 20:53:57 +0530

    Change name

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Kawaii kernel by Excraze & Xisan
+kernel.string=Kawaii kernel by Shikhar & Xisan
 do.devicecheck=1
 do.modules=0
 do.systemless=1
